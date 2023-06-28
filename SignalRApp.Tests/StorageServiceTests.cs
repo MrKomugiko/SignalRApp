056e77f6ef1bb03d355d111bd176099cfb8f62e0
MrKomugiko <Mr.Komugiko@gmail.com>
2023-06-29 00:39:47 +0200

    added simple tests

---
+++ SignalRApp.Tests/StorageServiceTests.cs
@@ -0,0 +1,22 @@
+using SignalRApp.Server.Data;
+
+namespace SignalRApp.Tests;
+
+public class StorageServiceTests
+{
+    [Fact]
+    public void Storage_Initialize_ShouldCountainsPopulatedItemsList()
+    {
+        Storage _storage = new();
+
+        Assert.NotEmpty(_storage.AllItems);
+    }
+
+    [Fact]
+    public void AllItems_AllItemsHasUniqueGuidId_ShouldBeTrue()
+    {
+        Storage _storage = new();
+
+        Assert.True(_storage.AllItems.DistinctBy(x=>x.Id).Count() == _storage.AllItems.Count);
+    }
+}
