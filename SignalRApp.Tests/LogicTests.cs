9128157cccf0499c11221b9ce0690bd561201592
MrKomugiko <Mr.Komugiko@gmail.com>
2023-07-01 22:03:02 +0200

    dodanie ważnego testu sprawdzającego magiczna liczbe 10 w algorytmie rozdzielenie workflow na 2  pliki

--- SignalRApp.Tests/LogicTests.cs
+++ SignalRApp.Tests/LogicTests.cs
@@ -16,7 +16,18 @@ public class LogicTests
         Assert.True(_logic.CustomAlgoritm(1) == 12);
         Assert.True(_logic.CustomAlgoritm(2) == 16);
         Assert.True(_logic.CustomAlgoritm(3) == 22);
+    }
 
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(3)]
+
+    public void CustomAlgoritm_magicNumberShouldBe10(int input)
+    {
+        ILogic _logic = new Logic();
+
+        Assert.Equal<int>(expected:10, actual:_logic.CustomAlgoritm(input)- (input*input + input));
     }
 
 }
