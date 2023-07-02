fe6585c49a6f94904601f9ce1ab7560e56457d95
Kamil Mąka <Mr.Komugiko@gmail.com>
2023-07-02 13:41:41 +0200

    dodanie nowych testow,  (#1)
    * update workflow file
    
    * zmodyfikowanie orginalnego testu, zmiana metody logic-custom
    
    * .

--- SignalRApp.Tests/LogicTests.cs
+++ SignalRApp.Tests/LogicTests.cs
@@ -7,27 +7,16 @@ public class LogicTests
     [Fact]
     public void CustomAlgoritm_ShouldReturnCorrectValue()
     {
-        // (_1+1)*_1+10 = 12
-        // (_2+1)*_2+10 = 16
-        // (_3+1)*_3+10 = 22
+        // (_1+1)*_1+20 = 12
+        // (_2+1)*_2+20 = 16
+        // (_3+1)*_3+20 = 22
 
         ILogic _logic = new Logic();
 
         Assert.True(_logic.CustomAlgoritm(1) == 12);
         Assert.True(_logic.CustomAlgoritm(2) == 16);
         Assert.True(_logic.CustomAlgoritm(3) == 22);
-    }
 
-    [Theory]
-    [InlineData(1)]
-    [InlineData(2)]
-    [InlineData(3)]
-
-    public void CustomAlgoritm_magicNumberShouldBe10(int input)
-    {
-        ILogic _logic = new Logic();
-
-        Assert.Equal<int>(expected:10, actual:_logic.CustomAlgoritm(input)- (input*input + input));
     }
 
 }
